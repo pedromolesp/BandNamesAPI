611abeac37ff3c49aab3f6442fe9923d0b4f1008
Pedro Molina <pedro.molina@petroprix.com>
2023-03-02 09:24:57 +0100

    terminada

--- sockets/socket.js
+++ sockets/socket.js
@@ -31,6 +31,10 @@ io.on('connection', client => {
 
         io.emit('active-bands', bands.getBands());
     })
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    })
     // client.on('emitir-mensaje', (payload) => {
     //     // console.log(payload);
     //     // io.emit('nuevo-mensaje', payload); //emite a todos
